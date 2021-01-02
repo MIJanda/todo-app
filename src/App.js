7b74679e8875bf025570fc334014fff3c2d65102
MIJanda <mokiliisaacjanda@gmail.com>
2021-01-02 12:36:36 +0300

    replaced <a> tags with <NavLink> tags

--- src/App.js
+++ src/App.js
@@ -8,14 +8,14 @@ import Contact from './components/Contact'
 class App extends Component {
     render() {
         return (
-            <div>
+            <BrowserRouter>
+            <div className="app">
                 <Navbar />
-                <BrowserRouter>
                     <Route exact path="/" component={Home} />
                     <Route path="/about" component={About} />
                     <Route path="/contact" component={Contact} />
-                </BrowserRouter>
             </div>
+            </BrowserRouter>
         )
     }
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,14 @@
+import { NavLink } from 'react-router-dom'
+
 const Navbar = () => {
     return (
         <nav className="navbar cyan">
             <div className="container">
-                <span className="navbar-brand white-text fs-1">Tasks</span>
-                <ul>
-                    <li className="nav-item"> <a href="/" className="nav-link">Home</a> </li>
-                    <li className="nav-item"> <a href="/about" className="nav-link">About</a> </li>
-                    <li className="nav-item"> <a href="/contact" className="nav-link">Contact</a> </li>
+                <span className="navbar-brand white-text fs-1 mb-4">Tasks</span>
+                <ul className="mb-2">
+                    <li className="nav-item"> <NavLink to="/" className="nav-link">Home</NavLink> </li>
+                    <li className="nav-item"> <NavLink to="/about" className="nav-link">About</NavLink> </li>
+                    <li className="nav-item"> <NavLink to="/contact" className="nav-link">Contact</NavLink> </li>
                 </ul>
             </div>
         </nav>
